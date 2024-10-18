c2ce9a138f31d1ddc1ad1334648521279a9730e9
msostaric031 <mark.sost@gmail.com>
2024-10-18 09:15:53 +0200

    Hero images

--- public/script.js
+++ public/script.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Select hamburger menu icon and menu content
   const hamburgerMenuIcon = document.querySelector(".hamburger-menu");
   const hamburgerMenuContent = document.getElementById("hamburgerMenu");
 
-  // Toggle the menu visibility when hamburger icon is clicked
   hamburgerMenuIcon.addEventListener("click", function () {
     hamburgerMenuContent.classList.toggle("open");
   });
