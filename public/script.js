887e0659d3fc469178a0c47ebfee0d5f69309439
msostaric031 <mark.sost@gmail.com>
2024-10-18 13:08:40 +0200

    Login screen and flow

--- public/script.js
+++ public/script.js
@@ -2,10 +2,42 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburgerMenuIcon = document.querySelector(".hamburger-menu");
   const hamburgerMenuContent = document.getElementById("hamburgerMenu");
 
+  const loginButtons = document.querySelectorAll(".login-btn-trigger");
+  const signInButton = document.querySelector(".login-screen-btn");
+  const bodyWrapper = document.querySelector(".body-wrapper");
+  const loginSection = document.querySelector("#login");
+  const header = document.querySelector(".header");
+
   const resourcesLink = document.querySelector(".header-resources");
   const dropdown = document.querySelector(
     ".header-resources-dropdown-container"
   );
+
+  loginButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      bodyWrapper.style.display = "none";
+      header.style.display = "none";
+
+      loginSection.style.display = "flex";
+    });
+  });
+
+  signInButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    if (email && password) {
+      loginSection.style.display = "none";
+      bodyWrapper.style.display = "block";
+      header.style.display = "flex";
+    } else {
+      alert("Please enter both email and password.");
+    }
+  });
+
+  hamburgerMenuContent.addEventListener("click", function () {});
 
   hamburgerMenuIcon.addEventListener("click", function () {
     hamburgerMenuContent.classList.toggle("open");
